2d9374bff6375c5b696be3860db1b23842ef03f2
mehtaash <mehtaash@014-1617-S14TS.students.cisd.conroeisd.loc>
2019-10-10 12:49:21 -0500

    10/10/19 in class changes

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) 
@@ -9,6 +11,7 @@ public class Main {
 	
 	public static void example1()
 	{
+		menu();
 		Bank bank = new Bank("Bank of CPHS");
 		int ewbankAccountNo = bank.createAccount("Mr. Ewbank");
 		int howardAccountNo = bank.createAccount("Mr. Howard");
@@ -25,5 +28,33 @@ public class Main {
 		bank.checkBalance(ewbankAccountNo);
 		bank.checkBalance(howardAccountNo);
 		
+	}
+	public static void menu() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to our bank! How can we help you?");
+		System.out.println("1: Deposit");
+		System.out.println("2: Withdraw");
+		System.out.println("3: Check");
+		System.out.println("4: Create");
+		System.out.println("5: Close");
+		System.out.println("6: Transfer");
+		System.out.println("7: Steal");
+		int choice = sc.nextInt();
+		switch(choice) {
+		case 1:
+			System.out.println("2: Withdraw");
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		}
+
+
+
+
+
+
 	}
 }
